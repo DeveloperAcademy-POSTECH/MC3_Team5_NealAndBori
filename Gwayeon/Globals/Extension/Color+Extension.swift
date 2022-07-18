15898ffc87c0da4f48982c66f66332b3970b686b
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-07-18 19:53:26 +0900

    [Add] Color Asset에 대한 UIColor Extension 추가

---
+++ Gwayeon/Globals/Extension/Color+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Exntension.swift
+//  Gwayeon
+//
+//  Created by yudonlee on 2022/07/18.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let pointColor = UIColor(ciColor: .red)
+    static let tabBarColor = UIColor(named: "TabBarColor")
+}
