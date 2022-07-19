97b145866525001e7af22867defdc97c46740fcd
Ayden <kaitaro77@naver.com>
2022-07-19 19:44:13 +0900

    [ADD] DetailView 문자, 전화 Button UI, Color Extension

---
+++ Gwayeon/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  Gwayeon
+//
+//  Created by Ayden on 2022/07/19.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let mainColor = UIColor(named: "mainColor")
+}
