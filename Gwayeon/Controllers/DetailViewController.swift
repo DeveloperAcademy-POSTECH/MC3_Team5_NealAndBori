c724934896ae279e5d417194b9d10b84b7646f91
Ayden <kaitaro77@naver.com>
2022-07-20 15:08:32 +0900

    [FEAT] 버튼 UI 구현

--- Gwayeon/Controllers/DetailViewController.swift
+++ Gwayeon/Controllers/DetailViewController.swift
@@ -45,6 +45,32 @@ class DetailViewController: UIViewController {
         return label
     }()
     
+    // UIButton.Configuration: ios 15에서 업데이트된 기능. more customizable.
+    private lazy var messageButton: UIButton = {
+        var config = UIButton.Configuration.plain()
+        config.cornerStyle = .capsule
+        config.background.strokeColor = .mainColor
+        config.baseForegroundColor = .mainColor
+        config.image = UIImage(systemName: "message")
+        config.title = " 문자"
+        config.attributedTitle?.font = .systemFont(ofSize: 17, weight: .bold)
+        config.buttonSize = .large
+        let button = UIButton(configuration: config)
+        return button
+    }()
+    
+    private lazy var callButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.cornerStyle = .capsule
+        config.baseBackgroundColor = .mainColor
+        config.image = UIImage(systemName: "phone.fill")
+        config.title = " 전화하기"
+        config.attributedTitle?.font = .systemFont(ofSize: 17, weight: .bold)
+        config.buttonSize = .large
+        let button = UIButton(configuration: config)
+        return button
+    }()
+    
     // MARK: Life Cycle Function
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -97,5 +123,26 @@ class DetailViewController: UIViewController {
             recommendLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
         ]
         NSLayoutConstraint.activate(recommendLabelConstraints)
+        
+        view.addSubview(messageButton)
+        messageButton.translatesAutoresizingMaskIntoConstraints = false
+        let messageButtonConstraints = [
+            messageButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5),
+            messageButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            messageButton.heightAnchor.constraint(equalToConstant: 56),
+            messageButton.widthAnchor.constraint(equalToConstant: view.frame.width * 0.35)
+        ]
+        NSLayoutConstraint.activate(messageButtonConstraints)
+        
+        view.addSubview(callButton)
+        callButton.translatesAutoresizingMaskIntoConstraints = false
+        let callButtonConstraints = [
+            callButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5),
+            callButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            callButton.leadingAnchor.constraint(equalTo: messageButton.trailingAnchor, constant: 8),
+            callButton.heightAnchor.constraint(equalToConstant: 56),
+            callButton.widthAnchor.constraint(equalToConstant: view.frame.width * 0.65)
+        ]
+        NSLayoutConstraint.activate(callButtonConstraints)
     }
 }
