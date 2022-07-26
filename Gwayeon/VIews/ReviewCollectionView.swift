0aed964026eb6682831014319c9a16f47cc5f668
Ayden <kaitaro77@naver.com>
2022-07-26 23:46:27 +0900

    [CHORE] 주석 제거, assert 대신 빈 셀 return

--- Gwayeon/VIews/ReviewCollectionView.swift
+++ Gwayeon/VIews/ReviewCollectionView.swift
@@ -9,18 +9,18 @@ import UIKit
 
 class ReviewCollectionView: UIView {
     
-//    private struct ReviewData {
-//        var date: String
-//        var contents: String
-//        var authors: String
-//    }
-//
-//    private var reviewData = [
-//        ReviewData(date: "22.07.21", contents: "세상 존맛탱. 이 세상 복숭아가 아니다. 과즙 팡팡이에요. 사장님도 정말 친절하시구요. 저만 먹기 너무 아까워서 모두에게 알려요.", authors: "쏘니"),
-//        ReviewData(date: "22.07.01", contents: "너무 맛있어요 짱짱 추천!", authors: "에이든"),
-//        ReviewData(date: "22.06.29", contents: "모두가 눈물 흘리며 먹을 복숭아맛. 여기가 바로 무릉도원!", authors: "메리"),
-//        ReviewData(date: "22.06.17", contents: "물복계의 성지입니다.", authors: "제리")
-//    ]
+    //    private struct ReviewData {
+    //        var date: String
+    //        var contents: String
+    //        var authors: String
+    //    }
+    //
+    //    private var reviewData = [
+    //        ReviewData(date: "22.07.21", contents: "세상 존맛탱. 이 세상 복숭아가 아니다. 과즙 팡팡이에요. 사장님도 정말 친절하시구요. 저만 먹기 너무 아까워서 모두에게 알려요.", authors: "쏘니"),
+    //        ReviewData(date: "22.07.01", contents: "너무 맛있어요 짱짱 추천!", authors: "에이든"),
+    //        ReviewData(date: "22.06.29", contents: "모두가 눈물 흘리며 먹을 복숭아맛. 여기가 바로 무릉도원!", authors: "메리"),
+    //        ReviewData(date: "22.06.17", contents: "물복계의 성지입니다.", authors: "제리")
+    //    ]
     
     private var date = ["22.07.21", "22.07.01", "22.06.29", "22.06.17"]
     private var contents = ["세상 존맛탱. 이 세상 복숭아가 아니다. 과즙 팡팡이에요. 사장님도 정말 친절하시구요. 저만 먹기 너무 아까워서 모두에게 알려요.", "너무 맛있어요 짱짱 추천!", "모두가 눈물 흘리며 먹을 복숭아맛. 여기가 바로 무릉도원!", "물복계의 성지입니다."]
@@ -89,9 +89,8 @@ extension ReviewCollectionView: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewCollectionViewCell.cellIdentifier, for: indexPath) as? ReviewCollectionViewCell else {
-            assert(false, "Wrong Cell")
-        }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewCollectionViewCell.cellIdentifier, for: indexPath) as? ReviewCollectionViewCell else { return UICollectionViewCell() }
+        
         cell.backgroundColor = .systemGray6
         cell.dateLabel.text = date[indexPath.item]
         cell.contentLabel.text = contents[indexPath.item]
