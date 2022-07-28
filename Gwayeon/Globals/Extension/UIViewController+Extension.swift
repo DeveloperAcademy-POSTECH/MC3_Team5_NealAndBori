c5f62657cffe9c984b140dccd8a526ad5de2f45f
YoonyounL <yyunglee97@gmail.com>
2022-07-28 12:37:21 +0900

    [Chore] Move keyboard extension to UIViewController+Extension file

---
+++ Gwayeon/Globals/Extension/UIViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Extension.swift
+//  Gwayeon
+//
+//  Created by 이윤영 on 2022/07/28.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
