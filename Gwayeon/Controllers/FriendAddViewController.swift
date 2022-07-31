0a2b25d1a271a772a2e1deec4a8ea1e6e2787338
hwiwonK <gnldnjs2469@gmail.com>
2022-08-01 02:26:48 +0900

    [Del] 과연 추가 화면 customNavigationBar 코드 삭 #105

--- Gwayeon/Controllers/FriendAddViewController.swift
+++ Gwayeon/Controllers/FriendAddViewController.swift
@@ -95,20 +95,6 @@ class FriendAddViewController: UIViewController {
         return button
     }()
     
-    private lazy var customNavigationBar: UINavigationBar = {
-        let navigationBar = UINavigationBar()
-        // 네비게이션 바 배경 색깔 제거, 강조색 설정
-        navigationBar.barTintColor = .systemBackground
-        navigationBar.tintColor = .mainRed
-        // 네비게이션 바 아래 구분선 제거
-        navigationBar.shadowImage = UIImage()
-        // 타이틀, 취소 버튼 추가
-        let navigationItem = UINavigationItem(title: "과연 추가")
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(closeModal))
-        navigationBar.items = [navigationItem]
-        return navigationBar
-    }()
-    
     @objc private func closeModal() {
         self.dismiss(animated: true, completion: nil)
     }
