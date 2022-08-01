2d756a526921966beb83a7ad3a114a222189343e
HeejiSohn <96990839+HeejiSohn@users.noreply.github.com>
2022-08-01 15:38:01 +0900

    [Fix] Change TabBarItem image and TabBarItem title to korean

--- Gwayeon/Controllers/MainTabBarViewController.swift
+++ Gwayeon/Controllers/MainTabBarViewController.swift
@@ -16,11 +16,12 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        homeViewController.tabBarItem.image = UIImage(systemName: "doc.text.image")
-        homeViewController.tabBarItem.title = "Home"
+   //     homeViewController.tabBarItem.image = UIImage(systemName: "doc.text.image")
+        homeViewController.tabBarItem.image = UIImage(systemName: "archivebox")
+        homeViewController.tabBarItem.title = "둘러보기"
         
         myPageViewController.tabBarItem.image = UIImage(systemName: "person")
-        myPageViewController.tabBarItem.title = "마이"
+        myPageViewController.tabBarItem.title = "내 과일"
         
         tabBar.tintColor = .red
         tabBar.backgroundColor = .tabBarColor
